c97ba6d158b91a03e8c28494bb239235d5eb406a
Abishek Padaki <abiks@Abisheks-MacBook-Air.local>
2023-05-05 15:04:06 -0700

    Updated create proposal method in frontend

--- frontend/app.js
+++ frontend/app.js
@@ -7,7 +7,7 @@ async function init() {
     const ganacheUrl = "http://localhost:7545"; // Replace with the URL of your Ganache instance
     web3 = new Web3(new Web3.providers.HttpProvider(ganacheUrl));
     const contractAbi = JSON.parse('[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"proposalId","type":"uint256"},{"indexed":false,"internalType":"string","name":"description","type":"string"}],"name":"ProposalCreated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"voter","type":"address"},{"indexed":false,"internalType":"uint256","name":"proposalId","type":"uint256"}],"name":"VoteCasted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"voterAddress","type":"address"}],"name":"VoterRegistered","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"proposals","outputs":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"voteCount","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[],"name":"proposalsCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"voters","outputs":[{"internalType":"bool","name":"isRegistered","type":"bool"},{"internalType":"bool","name":"hasVoted","type":"bool"},{"internalType":"uint256","name":"voteIndex","type":"uint256"}],"stateMutability":"view","type":"function","constant":true},{"inputs":[{"internalType":"address","name":"_voterAddress","type":"address"}],"name":"registerVoter","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_description","type":"string"}],"name":"createProposal","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_proposalId","type":"uint256"}],"name":"vote","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getResults","outputs":[{"components":[{"internalType":"uint256","name":"id","type":"uint256"},{"internalType":"string","name":"description","type":"string"},{"internalType":"uint256","name":"voteCount","type":"uint256"}],"internalType":"struct VotingSystem.Proposal[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function","constant":true}]')
-    const contractAddress = '0xceD2f965191D38738e86f79a6C4264ca60fEF2f4';
+    const contractAddress = '0x58677dA3642888A7438F801641581C1cfDd7d8B6';
 
     votingSystem = new web3.eth.Contract(contractAbi, contractAddress);
   web3.eth.defaultAccount = (await web3.eth.getAccounts())[0];
@@ -83,18 +83,21 @@ async function main() {
     });
 
     // Create Proposal
-    document.getElementById('create-proposal-btn').addEventListener('click', async () => {
-        const proposalName = document.getElementById('proposal-name').value;
-        const accounts = await web3.eth.defaultAccount;
+document.getElementById('create-proposal-btn').addEventListener('click', async () => {
+    const proposalName = document.getElementById('proposal-name').value;
+    const accounts = await web3.eth.defaultAccount;
 
-        try {
-            await votingSystem.methods.createProposal(proposalName).send({ from: accounts,  gas: 300000 });
-            console.log('Proposal created successfully');
-            updateProposalList();
-        } catch (err) {
-            console.error('Error creating proposal:', err);
-        }
-    });
+    try {
+        const createProposalResult = await votingSystem.methods.createProposal(proposalName).send({ from: accounts, gas: 300000 });
+        console.log('Proposal created successfully');
+        
+        // Wait for the transaction receipt and then update the proposal list
+        await updateProposalList();
+    } catch (err) {
+        console.error('Error creating proposal:', err);
+    }
+});
+
 
     // Cast Vote
     document.getElementById('cast-vote-btn').addEventListener('click', async () => {
